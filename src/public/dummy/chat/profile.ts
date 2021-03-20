d86beb188048238ce8f2172482dedcdb2c73f9b3
Otagera <Otagera@gmail.com>
2021-03-20 13:52:37 +0100

    random: Reduce ejs and increase ts but moving commented ejs to ts.

---
+++ src/public/dummy/chat/profile.ts
@@ -0,0 +1,171 @@
+//aboutprofile
+`
+	<!--
+	    <div class="mt-4">
+	        <p class="text-muted mb-1">Email</p>
+	        <h5 class="font-size-14">adc@123.com</h5>
+	    </div>
+
+	    <div class="mt-4">
+	        <p class="text-muted mb-1">Time</p>
+	        <h5 class="font-size-14">11:40 AM</h5>
+	    </div>
+
+	    <div class="mt-4">
+	        <p class="text-muted mb-1">Location</p>
+	        <h5 class="font-size-14 mb-0">California, USA</h5>
+	    </div>
+    -->
+`;
+//attachprofile
+`
+	<!--
+        <div class="card p-2 border mb-2">
+            <div class="d-flex align-items-center">
+                <div class="avatar-sm me-3 ms-0">
+                    <div class="avatar-title bg-soft-primary text-primary rounded font-size-20">
+                        <i class="ri-file-text-fill"></i>
+                    </div>
+                </div>
+                <div class="flex-1">
+                    <div class="text-start">
+                        <h5 class="font-size-14 mb-1">admin_v1.0.zip</h5>
+                        <p class="text-muted font-size-13 mb-0">12.5 MB</p>
+                    </div>
+                </div>
+
+                <div class="ms-4 me-0">
+                    <ul class="list-inline mb-0 font-size-18">
+                        <li class="list-inline-item">
+                            <a href="#" class="text-muted px-1">
+                                <i class="ri-download-2-line"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item dropdown">
+                            <a class="dropdown-toggle text-muted px-1" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="ri-more-fill"></i>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Delete</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="card p-2 border mb-2">
+            <div class="d-flex align-items-center">
+                <div class="avatar-sm me-3 ms-0">
+                    <div class="avatar-title bg-soft-primary text-primary rounded font-size-20">
+                        <i class="ri-image-fill"></i>
+                    </div>
+                </div>
+                <div class="flex-1">
+                    <div class="text-start">
+                        <h5 class="font-size-14 mb-1">Image-1.jpg</h5>
+                        <p class="text-muted font-size-13 mb-0">4.2 MB</p>
+                    </div>
+                </div>
+
+                <div class="ms-4 me-0">
+                    <ul class="list-inline mb-0 font-size-18">
+                        <li class="list-inline-item">
+                            <a href="#" class="text-muted px-1">
+                                <i class="ri-download-2-line"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item dropdown">
+                            <a class="dropdown-toggle text-muted px-1" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="ri-more-fill"></i>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Delete</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="card p-2 border mb-2">
+            <div class="d-flex align-items-center">
+                <div class="avatar-sm me-3 ms-0">
+                    <div class="avatar-title bg-soft-primary text-primary rounded font-size-20">
+                        <i class="ri-image-fill"></i>
+                    </div>
+                </div>
+                <div class="flex-1">
+                    <div class="text-start">
+                        <h5 class="font-size-14 mb-1">Image-2.jpg</h5>
+                        <p class="text-muted font-size-13 mb-0">3.1 MB</p>
+                    </div>
+                </div>
+
+                <div class="ms-4 me-0">
+                    <ul class="list-inline mb-0 font-size-18">
+                        <li class="list-inline-item">
+                            <a href="#" class="text-muted px-1">
+                                <i class="ri-download-2-line"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item dropdown">
+                            <a class="dropdown-toggle text-muted px-1" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="ri-more-fill"></i>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Delete</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="card p-2 border mb-2">
+            <div class="d-flex align-items-center">
+                <div class="avatar-sm me-3 ms-0">
+                    <div class="avatar-title bg-soft-primary text-primary rounded font-size-20">
+                        <i class="ri-file-text-fill"></i>
+                    </div>
+                </div>
+                <div class="flex-1">
+                    <div class="text-start">
+                        <h5 class="font-size-14 mb-1">Landing-A.zip</h5>
+                        <p class="text-muted font-size-13 mb-0">6.7 MB</p>
+                    </div>
+                </div>
+
+                <div class="ms-4 me-0">
+                    <ul class="list-inline mb-0 font-size-18">
+                        <li class="list-inline-item">
+                            <a href="#" class="text-muted px-1">
+                                <i class="ri-download-2-line"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item dropdown">
+                            <a class="dropdown-toggle text-muted px-1" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="ri-more-fill"></i>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Delete</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    -->
+`;
---
+++ src/public/dummy/main/groups.ts
@@ -0,0 +1,113 @@
+//after coming soon
+`
+<!-- Start chat-group-list --
+<div class="p-4 chat-message-list chat-group-list" data-simplebar>
+
+
+    <ul class="list-unstyled chat-list">
+        <li>
+            <a href="#">
+                <div class="d-flex align-items-center">
+                    <div class="chat-user-img me-3 ms-0">
+                        <div class="avatar-xs">
+                            <span class="avatar-title rounded-circle bg-soft-primary text-primary">
+                                G
+                            </span>
+                        </div>
+                    </div>
+                    <div class="flex-1 overflow-hidden">
+                        <h5 class="text-truncate font-size-14 mb-0">#General</h5>
+                    </div>
+                </div>
+            </a>
+        </li>
+
+        <li>
+            <a href="#">
+                <div class="d-flex align-items-center">
+                    <div class="chat-user-img me-3 ms-0">
+                        <div class="avatar-xs">
+                            <span class="avatar-title rounded-circle bg-soft-primary text-primary">
+                                R
+                            </span>
+                        </div>
+                    </div>
+                    <div class="flex-1 overflow-hidden">
+                        <h5 class="text-truncate font-size-14 mb-0">#Reporting <span class="badge badge-soft-danger rounded-pill float-end">+23</span></h5>
+                    </div>                        
+                </div>
+            </a>
+        </li>
+
+        <li>
+            <a href="#">
+                <div class="d-flex align-items-center">
+                    <div class="chat-user-img me-3 ms-0">
+                        <div class="avatar-xs">
+                            <span class="avatar-title rounded-circle bg-soft-primary text-primary">
+                                D
+                            </span>
+                        </div>
+                    </div>
+                    <div class="flex-1 overflow-hidden">
+                        <h5 class="text-truncate font-size-14 mb-0">#Designers</h5>
+                    </div>
+                </div>
+            </a>
+        </li>
+
+        <li>
+            <a href="#">
+                <div class="d-flex align-items-center">
+                    <div class="chat-user-img me-3 ms-0">
+                        <div class="avatar-xs">
+                            <span class="avatar-title rounded-circle bg-soft-primary text-primary">
+                                D
+                            </span>
+                        </div>
+                    </div>
+                    <div class="flex-1 overflow-hidden">
+                        <h5 class="text-truncate font-size-14 mb-0">#Developers <span class="badge badge-soft-danger rounded-pill float-end">New</span></h5>
+                    </div>
+                </div>
+
+            </a>
+        </li>
+
+        <li>
+            <a href="#">
+                <div class="d-flex align-items-center">
+                    <div class="chat-user-img me-3 ms-0">
+                        <div class="avatar-xs">
+                            <span class="avatar-title rounded-circle bg-soft-primary text-primary">
+                                P
+                            </span>
+                        </div>
+                    </div>
+                    <div class="flex-1 overflow-hidden">
+                        <h5 class="text-truncate font-size-14 mb-0">#Project-alpha</h5>
+                    </div>
+                </div>
+            </a>
+        </li>
+
+        <li>
+            <a href="#">
+                <div class="d-flex align-items-center">
+                    <div class="chat-user-img me-3 ms-0">
+                        <div class="avatar-xs">
+                            <span class="avatar-title rounded-circle bg-soft-primary text-primary">
+                                B
+                            </span>
+                        </div>
+                    </div>
+                    <div class="flex-1 overflow-hidden">
+                        <h5 class="text-truncate font-size-14 mb-0">#Snacks</h5>
+                    </div>
+                </div>
+            </a>
+        </li>
+    </ul>
+</div>
+-- End chat-group-list -->
+`;
